856a0e9156dadd3d421ae017f66c6ed9df515a08
PyScientist <dsa.msk@mail.ru>
2022-09-07 20:58:35 +0300

    add curve viewer part1

---
+++ application.py
@@ -0,0 +1,140 @@
+import sys
+import time
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QTextEdit
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget, QTableWidget, QTableView
+from PyQt5.QtWidgets import QDialog, QLineEdit, QComboBox, QLabel, QSpinBox
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+
+from las import CurveSet
+
+from matplotlib import pyplot as plt
+
+class ExplanationField:
+    """ExplanationField class"""
+
+    def __init__(self, host):
+        self.host = host
+        # Create widget and vertical layout for
+        # explanation information
+        self.expl_widget = QWidget()
+        self.container = QVBoxLayout()
+        self.expl_widget.setLayout(self.container)
+        self.textField = QTextEdit()
+        self.container.addWidget(self.textField)
+
+    def set_text(self, text):
+        """Inserting text to text field"""
+        self.textField.setText(text)
+
+    def add_text(self, text):
+        """Adding text to text field"""
+        self.textField.append(text)
+
+    def clear_text(self):
+        """Clear text in explanation field"""
+        self.textField.clear()
+
+
+class LButton:
+    """Button for lower panel"""
+
+    def __init__(self, action, text):
+        # Create font object and set and customize it
+        font = QtGui.QFont()
+        font.setPixelSize(50)
+        # Create button and set font to it
+        self.button = QPushButton(text)
+        self.button.setFont(font)
+        self.button.clicked.connect(action)
+
+
+class LSpin:
+    def __init__(self, default_value, name):
+        self.spinbox = QSpinBox()
+        self.spinbox.setRange(0, 1000)
+        self.spinbox.setValue(default_value)
+        self.spinbox.setObjectName(name)
+
+class MethodCombobox:
+    def __init__(self, values, name):
+        self.combobox_methods = QComboBox()
+        self.combobox_methods.addItems(values)
+        self.combobox_methods.setObjectName(name)
+
+
+class LowerButtonsPanel:
+    """Lower panel class"""
+
+    def __init__(self, host):
+        self.host = host
+        # Create widget and horizontal layout for
+        # lower panel for buttons which operate views
+        self.buttons_widget = QWidget()
+        self.container = QHBoxLayout()
+        self.buttons_widget.setLayout(self.container)
+
+    def add_combobox(self, values, name):
+        """"Add combobox with values"""
+        self.container.addWidget(MethodCombobox(values, name).combobox_methods)
+
+    def add_button(self, action, text):
+        """Add button to panel with action and text"""
+        self.container.addWidget(LButton(action=action, text=text).button)
+
+
+class MainWindow(QMainWindow):
+    
+    current_set = None
+        
+    def __init__(self):
+        super().__init__()
+        
+        def plot_curve():
+               x_index = None
+               y_index = None
+               for i in range(len(self.current_set.curves_list)):
+                   if self.current_set.curves_list[i] == 'GZ5':
+                       x_index = i
+                   if self.current_set.curves_list[i] == 'DEPT':
+                       y_index = i
+                                     
+               fig, ax = plt.subplots(1,1)
+               x = self.current_set.curves_data[x_index]
+               y = self.current_set.curves_data[y_index]
+               ax.plot(x, y)
+               plt.show()
+               
+        # Create widget and vertical layout.
+        # Set layout to widget
+        # Set widget as central widget of main window
+        self.CentrWidg = QWidget()
+        self.CentrLayout = QVBoxLayout()
+        self.CentrWidg.setLayout(self.CentrLayout)
+        self.setCentralWidget(self.CentrWidg)
+        
+        # Create explanation field and add it to central widget layout
+        self.expl_field = ExplanationField(self)
+        self.CentrLayout.addWidget(self.expl_field.expl_widget)
+        
+        # Create lower buttons panel (initially empty) and add it to central widget layout
+        self.lButtonsPanel = LowerButtonsPanel(self)
+        self.CentrLayout.addWidget(self.lButtonsPanel.buttons_widget)
+        
+        self.lButtonsPanel.add_button(plot_curve, 'plot chosen')
+        
+        las_path = './las_test/115_БКЗ.las'
+        self.current_set = CurveSet(las_path)
+        
+        self.expl_field.add_text(str(self.current_set.curves_list))
+        
+        
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
--- las.py
+++ las.py
@@ -1,51 +1,153 @@
-def read_file(las_path):
-    """Read file to following decomposition
-    return list of strings"""
-    file_to_procces = open(las_path, 'r')
-    strings_in_file = file_to_procces.readlines()
-    file_to_procces.close()
-    return strings_in_file
+import numpy as np
+from datetime import datetime
 
-
-def search_first_data_block_string(prefix, strings_in_file):
-    """Search the first string of data block with given prefix"""
-    position = 'Novalue'
-    for x in range(len(strings_in_file)):
-        if str(strings_in_file[x]).startswith(prefix) == 1:
-            print(strings_in_file[x].startswith(prefix))
-            position = x
-            break
-    return position
-
-
-def single_block(position, strings_in_file):
-    """"Combine single data block by its position"""
-    data_block = []
-    if (position)!='Novalue':
-        for x in range(position+1, len(strings_in_file)) :         
-            if strings_in_file[x].startswith("~") == 0:
-                if strings_in_file[x].startswith("#") == 0:
-                    data_block.append(strings_in_file[x].strip())
-            else:
-                break
-    return data_block
+class CurveSet:
+    strings_in_file = []
+    data_blocks_dict = {}
+    param_dict = {}
+    curves_list = []
+    units_list = []
+    description_list = []
+    curves_data  = np.array([])
+    logger = []
     
-    
-def combine_data_bloks(las_path):
-    """Prepare the set of datablocks"""
-    prefix_datablock_list=['~V', '~W', '~C', '~P', '~O', '~A', '~B']
-    strings_in_file  = read_file(las_path)
-    data_blocks = []
-    
-    for prefix in range(len(prefix_datablock_list)):
-        position = search_first_data_block_string(prefix_datablock_list[prefix], strings_in_file)
-        data_block = single_block(position, strings_in_file)
-        data_blocks.append(data_block)
-    print('exists')
-    return data_blocks
-    
+    def __init__(self, las_path):
+          self.las_path  = las_path
+          self.read_file()
+          self.combine_data_blocks()
+          self.version_search()
+          self.got_params()
+          self.get_curve_spec_list()
+          self.got_data()
+          self.fix_noval()
+          #self.printstat()
+          
+    def read_file(self):
+           """Read file to following decomposition
+           return list of strings"""
+           file_to_procces = open(self.las_path, 'r')
+           self.strings_in_file = file_to_procces.readlines()
+           file_to_procces.close()
+           
+    def search_first_data_block_string(self, prefix):
+            """Search the first string of data block with given prefix
+            and return it index (started from zero)"""
+            position = None
+            for x in range(len(self.strings_in_file)):
+                  if self.strings_in_file[x].startswith(prefix) == True:
+                     position = x
+                     break
+            return position
 
+    def single_block(self, position):
+            """"Combine single data block by its position"""
+            data_block = []
+            if (position)!=None:
+               for x in range(position+1, len(self.strings_in_file)):
+                     if self.strings_in_file[x].startswith("~") == False:
+                        if self.strings_in_file[x].startswith("#") == False:
+                           data_block.append(self.strings_in_file[x].strip())
+                     else: break
+                        
+            return data_block
+            
+    def combine_data_blocks(self):
+           """Prepare the set of datablocks by given prefixes"""
+           prefix_datablock_list=['~V', '~W', '~C', '~P', '~O', '~A', '~B']          
+           for prefix in prefix_datablock_list:
+                 self.data_blocks_dict[prefix] = []
+           for key in self.data_blocks_dict:                 
+                 position = self.search_first_data_block_string(key)               
+                 self.data_blocks_dict[key]=self.single_block(position)
+    
+    def printstat(self):
+           print(self.param_dict)
+           print(list(zip(self.curves_list, self.units_list, self.description_list)))
+    
+    def fix_noval(self):
+           self.curves_data[self.curves_data==-9999.25] = np.nan
+    
+    @staticmethod
+    def got_las_data(line, vers):
+           """Get data from las 2.0 version"""
+           length_line=len(line)
+           mesto_pervoi_tochki=line.find('.')
+           stroka_bez_nazvaniya=line[mesto_pervoi_tochki+1:length_line]
+           mesto_probela=stroka_bez_nazvaniya.find(' ')
+           mesto_dvoetochiya=stroka_bez_nazvaniya.find(':')
+           if (vers == '1.20') or (vers == '1.2') or (vers == '1.200'):
+              return line[line.find(':')+1:length_line].strip()
+           elif (vers == '2.0') or (vers == '2.00') or (vers == '2'):
+              return stroka_bez_nazvaniya[mesto_probela:mesto_dvoetochiya].strip()
+           else: return "missing"
+                                                                                                                                                                                                                                                                                                          
+    def version_search(self):
+           """Get las version"""           
+           for line in self.data_blocks_dict['~V']:
+                 if (line.startswith('VERS') == True) or (line.startswith('Vers') == True):
+                     self.param_dict['version'] = self.got_las_data(line, '2.0')
+                 if line.startswith('WRAP') == True:
+                    self.param_dict['wrap'] = self.got_las_data(line, '2.0')
+
+    def got_params(self):
+          """Get noval sign, start and stop depth from 
+          Well information block"""
+          self.param_dict['noval'] = 'missing'
+          self.param_dict['start'] = 'missing'
+          self.param_dict['stop'] = 'missing'
+          self.param_dict['step'] = 'missing'
+          self.param_dict['well'] = 'missing'
+          self.param_dict['uwi'] = 'missing'
+          self.param_dict['date'] = 'missing'
+
+          for line in self.data_blocks_dict['~W']:
+               if line.startswith('NULL') == True:
+                   self.param_dict['noval'] = self.got_las_data(line, '2')
+               elif line.startswith('STRT') == True:
+                   self.param_dict['start'] = self.got_las_data(line, '2')
+               elif line.startswith('STOP') == True:
+                   self.param_dict['stop'] = self.got_las_data(line, '2')
+               elif line.startswith('STEP') == True:
+                    self.param_dict['step'] = self.got_las_data(line, '2')
+               elif line.startswith('WELL') == True:
+                    self.param_dict['well'] = self.got_las_data(line, self.param_dict['version'])
+               elif line.startswith('UWI') == True:
+                    self.param_dict['uwi'] = self.got_las_data(line, self.param_dict['version'])
+               elif line.startswith('DATE') == True:
+                    self.param_dict['date'] = self.got_las_data(line, self.param_dict['version'])                                                                                                 
+
+    def get_curve_spec_list(self):
+           """Get logging list"""
+           for line in self.data_blocks_dict['~C']:
+                 self.curves_list.append(line[0:line.find(".")].strip())
+                 self.description_list.append(line[line.find(":")+1:len(line)].strip())
+                 self.units_list.append(line[line.find('.')+1:line.find(':')].strip())
+     
+    def got_data(self):
+           lines = [] 
+           for line in self.data_blocks_dict['~A']:
+                 current_line = line.split()
+                 if len(current_line) == len(self.curves_list):
+                    for i in range(len(current_line)):
+                        if current_line[i] == self.param_dict['noval']:
+                           current_line[i] = '-9999.25'
+                    lines.append(current_line)
+           true_lenght = float(self.param_dict['stop'])-float(self.param_dict['start'])
+           diff = (len(lines)-1)*float(self.param_dict['step']) - true_lenght
+           if diff == 0:
+               self.curves_data = np.array(lines).transpose().astype(np.float64)
+               self.logger.append('good quality')
+ 
+def perfomance_test(las_path):
+      for i in range(10):
+        start = datetime.now()
+        for x in range(80):
+              set1 = CurveSet(las_path)
+        print(datetime.now()-start)
+
+     
 if __name__ == '__main__':
      las_path = './las_test/115_БКЗ.las'
-     content = combine_data_bloks(las_path)
-     print(content)
+     perfomance_test(las_path)
+     #set1 = CurveSet(las_path)
+     
